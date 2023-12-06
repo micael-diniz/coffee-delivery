d5a7f2b79e101e4ad03cd7d2544038203e58c485
Micael Diniz <micaeldiniz6@gmail.com>
2023-12-06 20:02:42 -0300

    feat: add navigation to order placed

--- src/pages/Checkout/components/ConfirmOrder/index.tsx
+++ src/pages/Checkout/components/ConfirmOrder/index.tsx
@@ -1,6 +1,10 @@
+import { useNavigate } from 'react-router-dom'
+
 export function ConfirmOrder() {
+  const navigate = useNavigate()
   return (
     <button
+      onClick={() => navigate('/checkout/order-placed')}
       className={`rounded-[6px] bg-yellow-500 px-[0.8rem] py-[1.2rem] text-[1.4rem] font-bold uppercase text-white`}
     >
       Confirmar pedido
