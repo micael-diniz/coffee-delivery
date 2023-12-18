3eaf2d3d339d3161519cff6a3f250144f8be6eaf
Micael Diniz <micaeldiniz6@gmail.com>
2023-12-18 08:25:52 -0300

    feat: add items total to checkout summary

--- src/pages/Checkout/components/TotalItems/index.tsx
+++ src/pages/Checkout/components/TotalItems/index.tsx
@@ -1,6 +1,20 @@
+import { useMemo } from 'react'
 import { Price } from '../../../../components/Price'
+import { useOrder } from '../../../../contexts/OrderContext'
 
 export function TotalItems() {
+  const { cart } = useOrder()
+
+  const itemsTotal = useMemo(
+    () =>
+      cart.reduce((acc, { quantity, price }) => {
+        if (quantity) {
+          return acc + quantity * price
+        }
+        return acc + price
+      }, 0),
+    [cart],
+  )
   return (
     <div className={`flex justify-between`}>
       <span className={`text-[1.4rem] leading-[130%] text-gray-700`}>
@@ -8,7 +22,7 @@ export function TotalItems() {
       </span>
       <span>
         <Price
-          value={2970}
+          value={itemsTotal}
           priceStyle={`text-[1.6rem] text-gray-700 leading-[130%]`}
         />
       </span>
