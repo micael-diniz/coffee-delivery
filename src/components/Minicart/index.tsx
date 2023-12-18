ebe657b9d74af149e2c45b2ef07fbc397803356b
Micael Diniz <micaeldiniz6@gmail.com>
2023-12-17 23:41:09 -0300

    feat: update minicart total quantity

--- src/components/Minicart/index.tsx
+++ src/components/Minicart/index.tsx
@@ -2,15 +2,26 @@ import { Badge } from '@nextui-org/react'
 import { ShoppingCart } from 'phosphor-react'
 import { useNavigate } from 'react-router-dom'
 import { useOrder } from '../../contexts/OrderContext'
+import { useMemo } from 'react'
 
 export function Minicart() {
   const navigate = useNavigate()
   const { cart } = useOrder()
-  console.log({ cart })
+
+  const totalQuantity = useMemo(
+    () =>
+      cart.reduce((acc, { quantity }) => {
+        if (!quantity) return 0
+
+        return acc + quantity
+      }, 0),
+    [cart],
+  )
+
   return (
     <Badge
-      content="3"
-      className="border-none bg-yellow-700 p-[1rem] text-[1.2rem] font-bold text-white"
+      content={totalQuantity}
+      className="h-[2.4rem] w-[2.4rem] border-none bg-yellow-700 text-[1.2rem] font-bold text-white"
     >
       <button
         onClick={() => navigate('/checkout')}
